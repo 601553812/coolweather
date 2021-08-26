2d088060f5ca0becf854968011bd4b7fc73cf894
pxh <peixinhao1129@gmail.com>
2021-08-27 03:16:35 +0900

    加入显示天气信息的功能

--- app/src/main/java/com/hlju/coolweather/util/Utility.java
+++ app/src/main/java/com/hlju/coolweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.hlju.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.hlju.coolweather.db.City;
 import com.hlju.coolweather.db.County;
 import com.hlju.coolweather.db.Province;
+import com.hlju.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -67,5 +69,16 @@ public class Utility {
             }
         }
         return false;
+    }
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
